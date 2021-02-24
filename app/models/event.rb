757fd4804f70dabaf7009c81717b8a00e7de4ac3
saimaAnis21 <saima_rehman21@yahoo.com>
2021-02-24 15:42:15 +0500

    Add associations to the models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ApplicationRecord
+
+    has_many :eventattendees, foreign_key: :user_event_id
+    has_many :attendees, through: :eventattendees, source: :event_user
+    belongs_to :creator, class_name: "User"
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+
+    has_many :eventattendees, foreign_key: :event_user_id
+    has_many :user_events, through: :eventattendees
+    has_many :created_events, foreign_key: :creator_id, class_name: "Event"
+
 end
