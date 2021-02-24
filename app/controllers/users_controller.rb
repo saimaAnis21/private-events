e458d2ea6ab391d836cd080363911ed8c2167418
saimaAnis21 <saima_rehman21@yahoo.com>
2021-02-24 19:43:27 +0500

    Add sign in form and set the session variables

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
-  def new
+  def sign_in
+    @user = User.new
   end
 
-  def create
+  def logged_in
+    @user = User.where("user_name='#{params[:username]}'").take
+    session[:current_user_id] = @user.id
+    session[:current_user_name] = "#{@user.user_name}"
+    redirect_to users_show_path
   end
 
   def show
-    @user = User.all
+     @user = User.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new', to: 'users#show'
+  get 'users/sign_in', to: 'users#sign_in'
   get 'users/create', to: 'users#show'
   get 'users/show', to: 'users#show'
+  post 'users/sign_in', to: 'users#logged_in'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "users#show"
+  root "users#sign_in"
 end
