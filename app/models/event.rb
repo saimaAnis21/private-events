b1e292579206259ac6df5f16718bda73e9c1fd30
saimaAnis21 <saima_rehman21@yahoo.com>
2021-02-25 19:05:56 +0500

    Chnage event and user models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
 
-    # has_many :eventattendees, foreign_key: :user_event_id
-    # has_many :attendees, through: :eventattendees, source: :event_user
+    # has_many :event_attendees, foreign_key: :user_event_id
+    # has_many :attendees, through: :event_attendees, source: :event_user
     belongs_to :creator, class_name: "User"
 
 end
